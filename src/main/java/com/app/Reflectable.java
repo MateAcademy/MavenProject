cb9fa03f98fd29c326dce4c20282cfe263ebeac2
Sergey Klunniy <s.klunniy@gmail.com>
2019-12-24 13:25:48 +0200

    10) SimpleClass create

--- src/main/java/com/app/Reflectable.java
+++ src/main/java/com/app/Reflectable.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Reflectable {
-    public String nama();
+    public String name();
     public String value();
 
 }
---
+++ src/main/java/com/app/SimpleClass.java
@@ -0,0 +1,8 @@
+package com.app;
+
+/**
+ * @author Sergey Klunniy
+ */
+@Reflectable(name = "reflectable", value = "какие-то метаданные")
+public class SimpleClass {
+}
