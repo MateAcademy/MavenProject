df322d5af096689cab02600a0111331e03f3fc55
Sergey Klunniy <s.klunniy@gmail.com>
2019-12-27 01:15:19 +0200

    рабочая программа с сокетами

--- src/main/java/com/mate/Client.java
+++ src/main/java/com/mate/Client.java
@@ -1,8 +1,6 @@
 package com.mate;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.Socket;
 
 /**
@@ -12,10 +10,17 @@ public class Client {
     public static void main(String[] args) {
 
         try (Socket clientSocket = new Socket("localhost", 6666);
-             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()))
+//             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+             PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()), true);
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
           ) {
-            bufferedWriter.write("Hello, I'm client");
-            bufferedWriter.flush();
+//            bufferedWriter.write("Hello, I'm Сергей Клунный" + "\n");
+//            bufferedWriter.flush();
+            printWriter.println("Hello, I'm Сергей Клунный\n");
+//            printWriter.flush();
+
+            System.out.println("Ответ от сервера");
+            System.out.println(bufferedReader.read());
         }
         catch (IOException e) {
             e.printStackTrace();
--- src/main/java/com/mate/Server.java
+++ src/main/java/com/mate/Server.java
@@ -1,10 +1,9 @@
 package com.mate;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * @author Sergey Klunniy
@@ -13,9 +12,15 @@ public class Server {
     public static void main(String[] args) {
         try(ServerSocket serverSocket = new ServerSocket(6666);
             Socket socket = serverSocket.accept();
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
         ) {
-            System.out.println(bufferedReader.readLine());
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Ждем подключения и выводим");
+            String result = bufferedReader.readLine();
+            System.out.println(result);
+            bufferedWriter.write(scanner.next());
         } catch (IOException e) {
             e.printStackTrace();
         }
