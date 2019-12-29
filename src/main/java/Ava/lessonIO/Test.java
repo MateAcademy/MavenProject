088ca99b44077ba737bcf916562c6a1aa22f5bf5
Sergey Klunniy <s.klunniy@gmail.com>
2019-12-29 20:21:01 +0200

    java io продолжение

--- src/main/java/Ava/lessonIO/Test.java
+++ src/main/java/Ava/lessonIO/Test.java
@@ -2,8 +2,7 @@ package Ava.lessonIO;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 
 /**
  * @author Sergey Klunniy
@@ -11,16 +10,11 @@ import java.nio.file.Paths;
 public class Test {
     public static void main(String[] args) throws IOException {
 
-        Path path = Paths.get("Doc.txt");
+        Path path = Paths.get("src/Main/java/Ava/Doc1.txt");
+        Path path2 = Paths.get("src/Main/java/Ava/Doc2.txt");
+        Files.copy(path, path2, StandardCopyOption.COPY_ATTRIBUTES);
+        byte[] s = "Hello".getBytes();
+//        Files.write(path, s, StandardOpenOption.APPEND);
 
-
-        File file = new File("src/Main/java/Ava/Doc1.txt");
-        System.out.println(file.exists());
-        System.out.println(file.createNewFile());
-
-        System.out.println(file.getName());
-        System.out.println(file.getAbsolutePath());
-
-//        file.delete();
     }
 }
