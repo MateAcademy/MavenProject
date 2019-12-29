a6139980b4cbbea7af0bb7bc89af67b9622c0f6c
Sergey Klunniy <s.klunniy@gmail.com>
2019-12-29 14:41:26 +0200

    java io

--- src/main/java/Ava/lessonIO/Test.java
+++ src/main/java/Ava/lessonIO/Test.java
@@ -2,12 +2,17 @@ package Ava.lessonIO;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * @author Sergey Klunniy
  */
 public class Test {
     public static void main(String[] args) throws IOException {
+
+        Path path = Paths.get("Doc.txt");
+
 
         File file = new File("src/Main/java/Ava/Doc1.txt");
         System.out.println(file.exists());
