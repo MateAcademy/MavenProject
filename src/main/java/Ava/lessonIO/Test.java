b2b3748482c1afd1ca980e30340a894126ab4409
Sergey Klunniy <s.klunniy@gmail.com>
2019-12-29 13:56:44 +0200

    я создавал файлы

--- src/main/java/Ava/lessonIO/Test.java
+++ src/main/java/Ava/lessonIO/Test.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class Test {
     public static void main(String[] args) throws IOException {
         String fileName = "Doc5.txt";
-        String dirName = "lessonIO";
+        String dirName = "src";
         String path = dirName + File.separator + fileName;
         File file = new File(path);
         System.out.println(file.createNewFile());
