c7aef02d099c005f245106901ff19e58039803d0
Sergey Klunniy <s.klunniy@gmail.com>
2019-12-30 14:24:21 +0200

    hasNextDouble()

--- src/main/java/Ava2/SystemINDemo.java
+++ src/main/java/Ava2/SystemINDemo.java
@@ -1,14 +1,18 @@
 package Ava2;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * @author Sergey Klunniy
  */
 public class SystemINDemo {
     public static void main(String[] args) throws IOException {
-        int x = System.in.read();
-        char с = (char)x;
-        System.out.println("Код символа: " + с + " = " + x);
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNextDouble()) {
+            double i  = scanner.nextDouble();
+            System.out.println(i);
+        }
+
     }
 }
