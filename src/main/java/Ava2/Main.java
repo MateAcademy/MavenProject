17248c3609e3d16ac7539eabffd01594a5a6d109
Sergey Klunniy <s.klunniy@gmail.com>
2019-12-31 11:55:19 +0200

    работа с файлами BufferedReader

--- src/main/java/Ava2/Main.java
+++ src/main/java/Ava2/Main.java
@@ -1,21 +1,17 @@
 package Ava2;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * @author Sergey Klunniy
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File file = new File("temp.txt");
 
-        if (file.exists()) {
-            System.out.println("exists");
-        }
-        if (file.isDirectory()) {
-            System.out.println("Directory");
-        }
-        if (file.isFile()) {
+        if (!file.isFile()) {
+            file.createNewFile();
             System.out.println("File");
         }
     }
---
+++ src/main/java/Ava3/Main.java
@@ -0,0 +1,16 @@
+package Ava3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author Sergey Klunniy
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String s = bufferedReader.readLine();
+        System.out.println(s);
+    }
+}
