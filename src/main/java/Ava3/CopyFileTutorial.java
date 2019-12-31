8d1fd39bfef8c97d12f67555425eb0c73bd0c8b4
Sergey Klunniy <s.klunniy@gmail.com>
2019-12-31 14:56:16 +0200

    работа с файлами move

--- src/main/java/Ava3/CopyFileTutorial.java
+++ src/main/java/Ava3/CopyFileTutorial.java
@@ -11,14 +11,26 @@ import java.nio.file.StandardCopyOption;
 public class CopyFileTutorial {
     public static void main(String[] args) throws IOException {
 
-        copy();
+        System.out.println(moove());
     }
 
-    private static boolean copy()  {
+    private static boolean copy() {
         File file = new File("temp.txt");
         File file2 = new File("temp2.txt");
         try {
             Files.copy(file.toPath(), file2.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private static boolean moove() {
+        File file = new File("temp.txt");
+        File file2 = new File("D:\\projectsJAVA\\MavenProject\\src\\main\\java\\Ava3\\temp_copy.txt");
+        try {
+            Files.move(file.toPath(), file2.toPath());
             return true;
         } catch (IOException e) {
             e.printStackTrace();
