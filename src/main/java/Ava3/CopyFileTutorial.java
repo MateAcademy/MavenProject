e0c3d65d482636b0271e61977861b6adebdc270e
Sergey Klunniy <s.klunniy@gmail.com>
2019-12-31 14:39:15 +0200

    работа с файлами copy

--- src/main/java/Ava3/CopyFileTutorial.java
+++ src/main/java/Ava3/CopyFileTutorial.java
@@ -3,16 +3,26 @@ package Ava3;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 /**
  * @author Sergey Klunniy
  */
 public class CopyFileTutorial {
     public static void main(String[] args) throws IOException {
+
+        copy();
+    }
+
+    private static boolean copy()  {
         File file = new File("temp.txt");
-
-        Path path = file.toPath();
-        Files.createFile(path);
-
+        File file2 = new File("temp2.txt");
+        try {
+            Files.copy(file.toPath(), file2.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
