755df545ea90c81a44ace8261489f4750fdabf2a
Sergey Klunniy <s.klunniy@gmail.com>
2019-12-31 14:11:22 +0200

    работа с файлами BufferedReader 2)

--- src/main/java/Ava3/Main.java
+++ src/main/java/Ava3/Main.java
@@ -7,24 +7,11 @@ import java.io.*;
  */
 public class Main {
     public static void main(String[] args) throws IOException {
-        File file = new File("temp.txt");
 
-        FileWriter fileWriter = new FileWriter(file);
-//        fileWriter.write("Hello\n");
-//        fileWriter.write("Sergei\n");
-//        fileWriter.flush();
-//        fileWriter.close();
-
-        FileReader fileReader = new FileReader(file);
-        char[] chars = new char[21];
-        fileReader.read(chars);
-        System.out.println(chars);
-
-        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-        bufferedWriter.write("Tolik");
-        bufferedWriter.newLine();
-        bufferedWriter.write("Tolik");
-        bufferedWriter.flush();
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(new File("temp.txt")));
+        while (bufferedReader.ready()) {
+            System.out.println(bufferedReader.readLine() );
+        }
 
     }
 }
