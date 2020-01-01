d4bca9b99a7ce1e6dcaa014aaf4ffcc31de3e05a
Sergey Klunniy <s.klunniy@gmail.com>
2020-01-01 12:12:40 +0200

    я создал файл

--- src/main/java/Ava3/ava/Kitay.java
+++ src/main/java/Ava3/ava/Kitay.java
@@ -1,9 +1,12 @@
 package Ava3.ava;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Kitay {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File file = new File("temp.txt");
+        file.createNewFile();
+
     }
 }
