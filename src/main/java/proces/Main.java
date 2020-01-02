1e8c4c75a419f3c5a254ed231a889c75bf4a16ab
Sergey Klunniy <s.klunniy@gmail.com>
2020-01-02 11:08:57 +0200

    считываем из консоли и записываем в файл

--- src/main/java/proces/Main.java
+++ src/main/java/proces/Main.java
@@ -9,14 +9,24 @@ import java.util.Optional;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        File file = new File("D:\\projectsJAVA\\MavenProject\\src\\Doc1.txt");
-        try(FileReader fileReader = new FileReader(file);
-            BufferedReader bufferedReader = new BufferedReader(fileReader)
-        ) {
-            String line = null;
-            while ((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
+        try {
+            InputStreamReader inputStreamReader = new InputStreamReader(System.in); // поток чтения с консоли
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);  // соединяем InputStreamReader с BufferedReader
+
+            File file = new File("file.txt");
+            FileWriter fileWriter = new FileWriter(file); // поток, который подключается к текстовому файлу
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter); // соединяем FileWriter с BufferedWriter
+
+            String line;
+            while(!(line = bufferedReader.readLine()).equals("exit")) {
+                bufferedWriter.write(line);
+                bufferedWriter.newLine();
             }
+
+            bufferedReader.close(); // закрываем поток
+            bufferedWriter.close(); // закрываем поток
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
