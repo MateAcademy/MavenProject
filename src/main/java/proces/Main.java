32683f16cc2fec19ba7d6a6cf0ecabaa273ef77a
Sergey Klunniy <s.klunniy@gmail.com>
2020-01-02 10:53:14 +0200

    BufferedReader с консоли читаю

--- src/main/java/proces/Main.java
+++ src/main/java/proces/Main.java
@@ -1,9 +1,6 @@
 package proces;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.Optional;
 
 /**
@@ -12,10 +9,10 @@ import java.util.Optional;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        File file = new File("D:\\projectsJAVA\\MavenProject\\src\\Doc1.txt");
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-        while (bufferedReader.ready())
-        System.out.println(bufferedReader.readLine());
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String s = bufferedReader.readLine();
+        System.out.println(s);
 
     }
 }
